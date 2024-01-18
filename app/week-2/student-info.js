265743da84e4768f2b7cc1bc5b0e57ed87deb29f
Jayden <jayden.liwanag@edu.sait.ca>
2024-01-17 23:27:14 -0700

    Final Commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => 
+{
+  return (
+    <div>
+      <p>Jayden Liwanag</p>
+      <Link href="https://github.com/JaydenL-YVR/cprg306-assignments.git">https://github.com/JaydenL-YVR/cprg306-assignments.git</Link>
+    </div>
+  );
+};
+
+export default StudentInfo;
